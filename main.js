341c1a8e3f0815338489b70725671e5faf3d7d45
Asadkhan12334455 <160310609+Asadkhan12334455@users.noreply.github.com>
2024-04-05 22:37:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+console.log(chalk.redBright("Hello"));
+console.log("Guess a number between 1 to 6");
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: chalk.greenBright("Please guess anumber between 1-10")
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.blueBright("Congratualation! you gussed right number."));
+    console.log(chalk.blueBright("You won,"));
+}
+else {
+    console.log(chalk.redBright("Your are wrong number"));
+    console.log(chalk.redBright("Please try again"));
+}
